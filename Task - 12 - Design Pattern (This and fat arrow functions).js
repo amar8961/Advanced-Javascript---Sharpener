47c5b94963a93698e8c9ad371de44ab3a6841207
amar8961 <amarkumar8961@gmail.com>
2022-10-19 17:49:37 +0530

    5) call function to rescue

--- Task - 12 - Design Pattern (This and fat arrow functions).js
+++ Task - 12 - Design Pattern (This and fat arrow functions).js
@@ -1,9 +1,10 @@
 // 1) this inside a global scope
 this.table = 'window table'; 
 // console.log(window.table);  // output -> 'window table'
-const cleanTable = function(){  // 4) this inside a function
-    console.log(`cleaning ${this.table}`);
+const cleanTable = function(soap){  // 4) this inside a function
+    console.log(`cleaning ${this.table} using ${soap}`);
 }
+cleanTable.call(this,'some soap');  // 5) call function to rescue
 
 this.garage = {
     table: 'garage table',
@@ -21,3 +22,5 @@ let johnsRoom ={
 console.log(johnsRoom.table)
 johnsRoom.cleanTable()  // 3) this inside a method
 this.garage.cleanTable()  // 3) this inside a method
+cleanTable.call(this.garage,'some soap');  // 5) call function to rescue
+cleanTable.call(johnsRoom,'some soap');  // 5) call function to rescue
