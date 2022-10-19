c926b9cc53f9504152ac923c1af71f78d3ce3c8e
amar8961 <amarkumar8961@gmail.com>
2022-10-19 17:40:06 +0530

    this inside a method

--- Task - 12 - Design Pattern (This and fat arrow functions).js
+++ Task - 12 - Design Pattern (This and fat arrow functions).js
@@ -3,10 +3,18 @@ this.table = 'window table';
 // console.log(window.table);  // output -> 'window table'
 
 this.garage = {
-    table: 'garage table'
+    table: 'garage table',
+    cleanTable(){  // this inside a method
+        console.log(`cleaning ${this.table}`)
+    }
 };
 // this inside an object
 let johnsRoom ={
-    table: 'johns table'
+    table: 'johns table',
+    cleanTable(){  // this inside a method
+        console.log(`cleaning ${this.table}`)
+    }
 }
 console.log(johnsRoom.table)
+johnsRoom.cleanTable()  // this inside a method
+this.garage.cleanTable()  // this inside a method
