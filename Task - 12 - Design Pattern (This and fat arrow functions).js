661679e1246cfe3c201643e3e16e10b7fb0e375c
amar8961 <amarkumar8961@gmail.com>
2022-10-19 18:04:21 +0530

    6) 'this' inside an inner function

--- Task - 12 - Design Pattern (This and fat arrow functions).js
+++ Task - 12 - Design Pattern (This and fat arrow functions).js
@@ -1,26 +1,31 @@
-// 1) this inside a global scope
+// 1) 'this' inside a global scope
 this.table = 'window table'; 
 // console.log(window.table);  // output -> 'window table'
-const cleanTable = function(soap){  // 4) this inside a function
-    console.log(`cleaning ${this.table} using ${soap}`);
+const cleanTable = function(soap){  // 4) 'this' inside a function
+    
+    const innerFunction = function (_soap){  // 6) 'this' inside an inner function
+        console.log(`cleaning ${this.table} using ${_soap}`);
+    }
+    innerFunction(soap);  // 6) 'this' inside an inner function
 }
-cleanTable.call(this,'some soap');  // 5) call function to rescue
+
+// cleanTable.call(this,'some soap');  // 5) call function to rescue
 
 this.garage = {
     table: 'garage table',
-    cleanTable(){  // 3) this inside a method
-        console.log(`cleaning ${this.table}`)
-    }
+    // cleanTable(){  // 3) 'this' inside a method
+    //     console.log(`cleaning ${this.table}`)
+    // }
 };
-// 2) this inside an object
+// 2) 'this' inside an object
 let johnsRoom ={
     table: 'johns table',
-    cleanTable(){  // 3) this inside a method
-        console.log(`cleaning ${this.table}`)
-    }
+    // cleanTable(){  // 3) 'this' inside a method
+    //     console.log(`cleaning ${this.table}`)
+    // }
 }
-console.log(johnsRoom.table)
-johnsRoom.cleanTable()  // 3) this inside a method
-this.garage.cleanTable()  // 3) this inside a method
+// console.log(johnsRoom.table)  // 2) 'this' inside an object
+// johnsRoom.cleanTable()  // 3) 'this' inside a method
+// this.garage.cleanTable()  // 3) 'this' inside a method
 cleanTable.call(this.garage,'some soap');  // 5) call function to rescue
 cleanTable.call(johnsRoom,'some soap');  // 5) call function to rescue
