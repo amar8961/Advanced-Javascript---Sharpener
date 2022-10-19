596ed4dc66c9434e8cebee2ac87932b6e264ea00
amar8961 <amarkumar8961@gmail.com>
2022-10-19 17:34:24 +0530

    this inside an object

--- Task - 12 - Design Pattern (This and fat arrow functions).js
+++ Task - 12 - Design Pattern (This and fat arrow functions).js
@@ -1,3 +1,12 @@
 // this inside a global scope
 this.table = 'window table'; 
-console.log(window.table);  // output -> 'window table'
+// console.log(window.table);  // output -> 'window table'
+
+this.garage = {
+    table: 'garage table'
+};
+// this inside an object
+let johnsRoom ={
+    table: 'johns table'
+}
+console.log(johnsRoom.table)
